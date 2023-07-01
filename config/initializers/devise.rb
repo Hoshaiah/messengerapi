614ee699e5cacb29a5803b96f68056eb64c96cb4
Hosh <domingohoshaiah@gmail.com>
2023-07-02 01:47:21 +0800

    added unti before jsonapi-serializer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -264,6 +264,18 @@ Devise.setup do |config|
   #
   # The "*/*" below is required to match Internet Explorer requests.
   # config.navigational_formats = ['*/*', :html, :turbo_stream]
+  config.navigational_formats = []
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.jwt do |jwt|
+    jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
+    jwt.dispatch_requests = [
+      ['POST', %r{^/login$}]
+    ]
+    jwt.revocation_requests = [
+      ['DELETE', %r{^/logout$}]
+    ]
+    jwt.expiration_time = 30.minutes.to_i
+  end
 
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
