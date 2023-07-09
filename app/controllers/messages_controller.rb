a06c18909a1f1c4322d50e348469be92a99a31f6
Hosh <domingohoshaiah@gmail.com>
2023-07-09 17:35:04 +0800

    set up restrictions to retrieving channel data for users

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,18 +1,29 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: %i[ show update destroy ]
-  before_action :authenticate_user, only: %i[ create ]
+  before_action :authenticate_user, only: [:index, :create]
 
   # GET /messages
   def index
-    @messages = Message.all
-
+    # @messages = Message.all
+    if params['recipient_type'] == 'User'
+      @messages = Message.where(
+        recipient_id: current_user.id,
+        recipient_type: params['recipient_type'],
+        sender_id: params['sender_id']
+      )
+    elsif params['recipient_type'] == 'Channel'
+      @messages = Message.where(
+        recipient_id: params['recipient_id'],
+        recipient_type: params['recipient_type']
+      )
+    end
     render json: @messages
   end
 
   # GET /messages/1
-  def show
-    render json: @message
-  end
+  # def show
+  #   render json: @messages
+  # end
 
   # POST /messages
   def create
@@ -31,18 +42,18 @@ class MessagesController < ApplicationController
   end
 
   # PATCH/PUT /messages/1
-  def update
-    if @message.update(message_params)
-      render json: @message
-    else
-      render json: @message.errors, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   if @message.update(message_params)
+  #     render json: @message
+  #   else
+  #     render json: @message.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # DELETE /messages/1
-  def destroy
-    @message.destroy
-  end
+  # def destroy
+  #   @message.destroy
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -52,7 +63,7 @@ class MessagesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def message_params
-      params.require(:message).permit(:body, :recipient_id, :recipient_type)
+      params.require(:message).permit(:body, :recipient_id, :recipient_type, :sender_id)
     end
 
     def authenticate_user
@@ -61,12 +72,33 @@ class MessagesController < ApplicationController
         current_user = User.find(jwt_payload['sub'])
       end
       
-      # byebug
       if !current_user || jwt_payload['jti'] != current_user.jti
         render json: {
           status: 404,
           message: "You do not have permission to do this action."
         }
+        return
+      end
+
+
+      if params['recipient_type'] == 'Channel'
+        channel = Channel.find_by(id: params['recipient_id'])
+        if channel.nil?
+          render json: {
+            status: 404,
+            message: "Channel does not exist."
+          } 
+          return
+        end
+
+
+        if !channel.users.ids.include? current_user.id
+        render json: {
+          status: 404,
+          message: "User is not part of this channel"
+        }
+        return
+        end
       end
     end
 end
