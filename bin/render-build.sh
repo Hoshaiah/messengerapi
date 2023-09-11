296e3bfe800d9821ae60af516954ee7603cdc687
Hosh <domingohoshaiah@gmail.com>
2023-09-12 04:23:07 +0800

    commented out new sh commands

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,9 +2,9 @@
 # exit on error
 set -o errexit
 
-bundle config unset deployment
-bundle lock --add-platform ruby
-bundle lock --add-platform x86_64-linux
+# bundle config unset deployment
+# bundle lock --add-platform ruby
+# bundle lock --add-platform x86_64-linux
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
