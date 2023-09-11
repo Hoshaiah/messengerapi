8e1851a1b8c3507f028d6cc5d623d2e9134c1815
Hosh <domingohoshaiah@gmail.com>
2023-09-12 04:29:00 +0800

    uncommented out linux

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 # bundle config unset deployment
 # bundle lock --add-platform ruby
-# bundle lock --add-platform x86_64-linux
+bundle lock --add-platform x86_64-linux
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
