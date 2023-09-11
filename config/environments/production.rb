97a350748893384a525648bb71902dbfb6687dc1
Hosh <domingohoshaiah@gmail.com>
2023-09-12 07:42:38 +0800

    made changes to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,8 +34,8 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = nil
-  config.action_cable.url = "wss://hosh-messenger-api.onrender.com.com/cable"
+  config.action_cable.mount_path = '/cable'
+  config.action_cable.url = "wss://hosh-messenger-api.onrender.com.com/"
   config.action_cable.allowed_request_origins = [ "*", "http://example.com", /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
